80243bbe5d3f13bed9b038703d3a46e512386e24
soichiro <sou412snow@gmail.com>
2020-06-02 19:31:18 -0400

    functional EM

--- R/anova_logit.R
+++ R/anova_logit.R
@@ -16,10 +16,18 @@ anova_logit <- function(y, X, option = list()) {
   ## EM
   params <- al_em_run(y, X, option)
 
+
+  ## fitted value
+  betas  <- params[[1]][['beta']]
+  fitted <- 1 / (1 + exp(-as.vector(X %*% betas)))
+
   ## return estimates
-  attr(params, "option") <- option
-  class(params) <- c("anova_logit")
-  return(params)
+  option$iteration   <- params$iteration
+  option$convergence <- params$convergence
+  attr(betas, "option") <- option
+  attr(betas, "fitted") <- fitted
+  class(betas) <- c("anova_logit")
+  return(betas)
 }
 
 
@@ -27,26 +35,54 @@ anova_logit <- function(y, X, option = list()) {
 #' @keywords internal
 al_estep <- function(X, betas) {
   Xb <- X %*% betas
-  omega <- tanh(Xb / 2) / (2 * Xb)
+  omega <- as.vector(tanh(Xb / 2) / (2 * Xb))
   return(omega)
 }
 
 #' M-step function to solve quadratic programming.
 #' @keywords internal
-al_mstep <- function(y, X, omega) {
+al_mstep <- function(y, X, omega, p_vec) {
 
 
   ## prepare inputs
   Dmat <- t(X) %*% diag(omega) %*% X
-  dvec <- t(X) %*% ((Dmat - 1/2) / omega)
-  Amat
+  # Dmat <- Matrix::nearPD(t(X) %*% diag(omega) %*% X)$mat
+  dvec <- as.vector(t(X) %*% diag(omega) %*% ((y - 1/2) / omega))  ## change this to accomodate binomial cases n[i] > 1
+  Amat <- create_Amat(pvec = p_vec); Amat[1,1] <- 0
+  bvec <- rep(0, length(p_vec))
 
   ## solve QP
   ## can we pass the initial base?
-  fit <- quadprog::solve.QP(Dmat, dvec, Amat)
+  # fit <- quadprog::solve.QP(Dmat, dvec, Amat)
+  settings <- osqp::osqpSettings(verbose = FALSE, eps_abs = 1e-8, eps_rel = 1e-8)
+  osqp_solution <- osqp::solve_osqp(
+      Dmat, -dvec, Amat, l = bvec, u = bvec, pars = settings)
+
+  return(osqp_solution$x)
 
 }
 
+
+#' Create A matrix for linear constraints
+#' @keywords internal
+create_Amat <- function(pvec) {
+
+  n_rows <- length(pvec)
+  n_cols <- sum(pvec)
+  Amat   <- matrix(0, nrow = n_rows, ncol = n_cols)
+
+  lb <- 1; ub <- pvec[1]
+  for (i in seq_len(n_rows)) {
+    Amat[i,lb:ub] <- 1
+    ## update lower bound
+    lb <- lb + pvec[i]
+    ## update upper bound
+    if (i < n_rows) ub <- ub + pvec[i+1]
+  }
+
+  return(Amat)
+
+}
 
 #' EM-algorithm for the ANOVA logit
 #' @keywords internal
@@ -54,21 +90,21 @@ al_em_run <- function(y, X, option) {
 
   ## initialize parameters
   params <- vector("list", length = option$max_iter)
-  params <- al_params_initialize(params)
+  params <- al_params_initialize(X, params)
 
   ##
   ## Run EM algorithm
   ##
-  for (iter in seq(2, option$max_iter, 1)) {
+  for (iter in seq_len(option$max_iter)) {
     ## E-step ---------------------------------------------
     params[[iter]]$omega <- al_estep(X, params[[iter]]$beta)
 
     ## M-step ---------------------------------------------
-    params[[iter+1]]$beta  <- al_mstep(X, params[[iter]]$omega)
+    params[[iter+1]]$beta  <- al_mstep(y, X, params[[iter]]$omega, option$pvec)
 
     ## check_convergence
     if (iter > 1 &&
-        al_check_convergence(params[c(iter - 1, iter)]) < tol) {
+        al_check_convergence(params[c(iter - 1, iter)]) < option$tol) {
         break;
     }
   }
@@ -76,6 +112,9 @@ al_em_run <- function(y, X, option) {
 
   ## remve NULL elements
   par <- params[purrr::map_lgl(params, ~!is.null(.x))]
+  par <- par[length(par)]
+  par$iteration <- iter
+  par$convergence <- if_else(iter < option$max_iter, 1, 0)
   return(par)
 }
 
@@ -107,4 +146,17 @@ al_check_convergence <- function(params) {
 al_params_initialize <- function(X, params) {
   params[[1]]$beta <- rnorm(ncol(X), 0, 0.5)
   return(params)
+}
+
+
+
+#' Compute predicted probabilities
+#' @export
+predict.anova_logit <- function(fit, newdata = NULL) {
+  if (is.null(newdata)) {
+    fitted <- attr(fit, "fitted")
+  } else {
+    fitted <- 1 / (1 + exp(-newdata %*% fit))
+  }
+  return(fitted)
 }
