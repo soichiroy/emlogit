8b76caa90428e010ff658577c70f9e7d63e3cf6f
soichiro <sou412snow@gmail.com>
2020-06-16 11:13:03 -0400

    Add CBSW script

---
+++ R/cbsw.R
@@ -0,0 +1,28 @@
+
+##
+## Covariate Balancing Survey Weights with Categorical Variables
+##
+
+
+
+#' Covariate Balancing Survey Weights for Categorical Variables
+#' @export
+CBSW <- function(formula, data, option) {
+
+}
+
+
+
+create_input_matrix <- function() {
+  ##
+  ## ADMM formulation to solve the following problem
+  ##
+  ##         minimize     f(β) + g(η)
+  ##        subject to    Aβ + Bη = c
+  ##
+
+
+  ## create A
+
+  ## create B 
+}
