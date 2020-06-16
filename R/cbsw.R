f514ea8f8a5e9ec45f6c6221084b1b33fa95b5f7
soichiro <sou412snow@gmail.com>
2020-06-16 12:24:32 -0400

    Add input function

--- R/cbsw.R
+++ R/cbsw.R
@@ -12,8 +12,12 @@ CBSW <- function(formula, data, option) {
 }
 
 
-
-create_input_matrix <- function() {
+#' Create Matrix Inputs
+#' @import Matrix
+#' @importFrom igraph graph_from_adjacency_matrix
+#' @importFrom genlasso getDgSparse
+#' @keywords internal
+create_input_matrix <- function(Lk_list) {
   ##
   ## ADMM formulation to solve the following problem
   ##
@@ -23,6 +27,59 @@ create_input_matrix <- function() {
 
 
   ## create A
+  Ak <- D <- dvec <- list()
+  Ak[[1]] <- Matrix(0, nrow = 1, ncol = 1)
 
-  ## create B 
+  for (k in seq_len(length(Lk_list))) {
+    Lk        <- Lk_list[[k]]
+
+    ## Create differencing matrix
+    Adj       <- matrix(1, nrow = Lk, ncol = Lk)
+    tmp_graph <- igraph::graph_from_adjacency_matrix(Adj, mode = 'undirected', diag = FALSE)
+    Dk        <- genlasso::getDgSparse(tmp_graph)
+
+    ## Create ones and zeros
+    ones  <- Matrix(1, nrow = 1, ncol = Lk, sparse = TRUE)
+
+    ## Combine all
+    ## Ak = | 1 |
+    ##      | D |
+    ##
+    Ak[[k+1]] <- rbind(ones, Dk)
+    D[[k]]    <- Dk
+    dvec[[k]] <- nrow(Dk)
+  }
+
+  Amat <- bdiag(Ak)
+
+  ## create B
+  ## B = | 0 |
+  ##     | I |
+  Bk <- list()
+  for (k in seq_len(length(Lk_list))) {
+    Lk <- Lk_list[[k]]
+    zeros_Lk <- Matrix(0, nrow = 1, ncol = Lk)
+    Bk[[k]]  <- rbind(zeros_Lk, Diagonal(Lk))
+  }
+
+  Bmat <- bdiag(Bk)
+  return(list(A = Amat, B = Bmat, Ak = Ak, Bk = Bk, D = D, d = dvec))
+}
+
+
+
+#' Prepare parameter Inputs
+#' @param obj An output object from \code{create_input_matrix}.
+#' @import Matrix
+#' @keywords internal
+cbsw_prepare_params <- function(obj) {
+
+  ## Prepare β for f(β)
+  beta_param <- rnorm(ncol(obj$A))
+
+  ## Prepare η for g(η)
+  eta_param  <- rnorm(ncol(obj$B))
+
+
+  return(list(beta = beta_param, eta = eta_param))
 }
