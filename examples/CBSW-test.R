ba237cb46e2797d950892261a54a8ef0e50ed05a
sou412 <sou412snow@gmail.com>
2020-06-24 12:05:32 -0400

    Update test scripts

--- examples/CBSW-test.R
+++ examples/CBSW-test.R
@@ -23,29 +23,42 @@ ct <- create_input_matrix(as.list(xx$pvec))
 pm <- cbsw_prepare_params(ct)
 
 
-ct$lambda  <- 0.5
+ct$lambda  <- 0.01
 ct$rho     <- 1
 
-debugonce(cbsw_admm)
-debugonce(cbsw_update_beta)
-fit <- cbsw_admm(pm, xx, ct, option = list(max_iter = 10, tol = 1e-5,
-  use_grad = FALSE))
 
-pm$beta <- fit$beta
 
-y <- ct$B %*% pm$eta + pm$w
-Ab <- ct$A %*% pm$beta
-Xb <- xx$X %*% pm$beta
-S  <- xx$y
-obj_main <- sum(-S * exp(-Xb) - (1 - S) * Xb)
-obj_pen  <- as.vector(ct$rho/2 * (t(Ab) %*% Ab + 2 * t(y) %*% Ab))
+# debugonce(cbsw_admm)
+# debugonce(cbsw_update_beta)
+fit <- cbsw_admm(pm, xx, ct, option = list(max_iter = 30, tol = 1e-5,
+  use_grad = TRUE))  
 
-debugonce(cbsw_main_objective_fn)
-cbsw_main_objective_fn(
-  par = pm$beta, S = xx$y, X = xx$X, params = pm,
-  A = ct$A, B = ct$B, rho = ct$rho)
 
-debugonce(cbsw_main_objective_gr)
-cbsw_main_objective_gr(
-  par = pm$beta, S = xx$y, X = xx$X, params = pm,
-  A = ct$A, B = ct$B, rho = ct$rho)
+weights <- exp(-xx$X %*% fit$beta)
+
+cbind(
+colSums(xx$X[xx$y == 1, ] * weights[xx$y == 1]) / sum(xx$y == 0),
+colMeans(xx$X[xx$y == 0, ])
+)
+
+
+# debugonce(cvCBSW)
+set.seed(1234)
+cv_fit <- cvCBSW(
+    vv_turnout ~  pid3 + newsint,
+    data = nc_sample %>%
+      mutate(pid3 = as_factor(pid3), 
+             newsint = as_factor(newsint)), 
+    option = list()         
+)
+
+
+
+coef_mat <- map(cv_fit, ~.x$beta) %>% 
+  do.call(rbind, .) %>% 
+  data.matrix()
+  
+par(mfrow = c(1, 3))
+matplot(coef_mat[,1], type = 'l')  
+matplot(coef_mat[,2:6], type = 'l')  
+matplot(coef_mat[,7:11], type = 'l')  
